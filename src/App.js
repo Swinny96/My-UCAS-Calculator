9e42d066f4789b685dc247c093c127db64ad49fe
Christopher Swinton <33087553+Swinny96@users.noreply.github.com>
2021-01-26 13:20:32 +0000

    Structure Test

--- src/App.js
+++ src/App.js
@@ -1,25 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import styled from "styled-components";
+import "./App.css";
+import UCASCalculator from "./Components/UCAS-Calculator";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container>
+      <Header>
+        <Logo src={logo} />
+        <Heading>Calculator</Heading>
+      </Header>
+      <Content>
+        <Context>
+          Working out your UCAS points can be a pain - especially when you have
+          different types of qualifications. Luckily, our calculator is here to
+          help. Just add your qualifications and let our UCAS Calculator do the
+          maths!
+        </Context>
+        <UCASCalculator />
+      </Content>
+    </Container>
   );
 }
 
 export default App;
+
+const Container = styled.div`
+  margin: 24px;
+`;
+const Header = styled.header`
+  display: inline-flex;
+  margin-bottom: 20px;
+`;
+const Heading = styled.h1`
+  margin: 0px 20px;
+`;
+const Logo = styled.img`
+  height: 40px;
+`;
+const Content = styled.main``;
+const Context = styled.p`
+  max-width: 600px;
+`;
